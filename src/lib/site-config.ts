1f170a8adefc4d77fbf8038093b36ab0c6bd8982
Fardeen Ehsan <fardeen.es7@gmail.com>
2026-01-28 22:44:12 +0600

    feat: delete the entire client application codebase

--- src/lib/site-config.ts
+++
@@ -1,5 +0,0 @@
-export const SiteConfig = {
-  title: 'LimeFolio',
-  description: 'Showcase Your Creative Journey',
-  version: '0.0.1'
-};
--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-export { auth as middleware } from '@/auth';
-
-// Don't invoke Middleware on some paths
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
-};
