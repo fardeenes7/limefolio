9b1dd87cf61b14782ec9f673b2a506311ea863af
Fardeen Ehsan <fardeen.es7@gmail.com>
2026-02-04 13:16:44 +0600

    feat: integrate Google Tag Manager using the `@next/third-parties` package.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -55,6 +56,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={outfit.variable}>
+            <GoogleTagManager gtmId="GTM-PDSBXRH5" />
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
